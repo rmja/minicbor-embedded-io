d527387d8e374147eb469c5a7cb7a531f1ced54e
Rasmus Melchior Jacobsen <rmja@laesoe.org>
2023-02-10 12:25:55 +0100

    Add test

--- src/reader.rs
+++ src/reader.rs
@@ -12,6 +12,7 @@ where
     decoded: usize,
 }
 
+#[derive(Debug)]
 pub enum Error {
     UnexpectedEof,
     Io(embedded_io::ErrorKind),
@@ -70,13 +71,15 @@ where
                 self.read += len;
             }
 
+            let mut decoder = Decoder::new(&self.buf[self.decoded..self.read]);
             let decoded: Option<T> =
-                Self::try_decode_with(&self.buf[self.decoded..self.read], ctx)?;
+                Self::try_decode_with(&mut decoder, ctx)?;
             if decoded.is_some() {
+                self.decoded += decoder.position();
                 return Ok(decoded);
             }
 
-            // Remove the just decoded value from the buffer by moving the
+            // Remove the decoded values from the buffer by moving the
             // remaining, unused bytes in the buffer to the beginning
             self.buf.copy_within(self.decoded..self.read, 0);
             self.read -= self.decoded;
@@ -85,11 +88,9 @@ where
     }
 
     fn try_decode_with<'a, C, T: Decode<'a, C>>(
-        buf: &'a [u8],
+        decoder: &mut Decoder<'a>,
         ctx: &mut C,
     ) -> Result<Option<T>, Error> {
-        let mut decoder = Decoder::new(buf);
-
         match decoder.decode_with(ctx) {
             Ok(decoded) => Ok(Some(decoded)),
             Err(e) if e.is_end_of_input() => Ok(None),
@@ -98,8 +99,29 @@ where
     }
 }
 
+#[derive(Debug)]
+pub struct Array(pub Option<u64>);
+
+impl<'b, C> Decode<'b, C> for Array {
+    fn decode(d: &mut Decoder<'b>, ctx: &mut C) -> Result<Self, decode::Error> {
+        Ok(Array(d.array()?))
+    }
+}
+
 #[cfg(test)]
 mod tests {
-    #[test]
-    fn can_read() {}
+    use super::*;
+
+    #[tokio::test]
+    async fn can_read() {
+        let mut buf = [0; 16];
+        let cbor: [u8; 4] = [0x83, 0x01, 0x02, 0x03];
+        let mut reader = CborReader::new(cbor.as_slice(), &mut buf);
+        assert_eq!(3, reader.read::<Array>().await.unwrap().unwrap().0.unwrap());
+
+        assert_eq!(1, reader.read::<u8>().await.unwrap().unwrap());
+        assert_eq!(2, reader.read::<u8>().await.unwrap().unwrap());
+        assert_eq!(3, reader.read::<u8>().await.unwrap().unwrap());
+        assert!(reader.read::<u8>().await.unwrap().is_none());
+    }
 }
